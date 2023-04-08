57ea4f993746cf964abf8bfb55747bcbe3abc795
Lacey Weaver <jlweave89@gmail.com>
2023-04-08 14:21:41 -0600

    Add models-relationships and tests for them

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'relationship' do
+    it { should have_many(:subscriptions) }
+    it { should have_many(:customers).through(:subscription) }
+  end
 end
