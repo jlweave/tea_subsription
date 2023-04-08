952829724c034c923aa8947d8aee51c4179743f5
Lacey Weaver <jlweave89@gmail.com>
2023-04-08 14:23:02 -0600

    Add update to schema and factory bot

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :subscription do
+    association :tea
+    association :customer
     title { Faker::Subscription.plan }
     price { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
     status { Faker::Subscription.status }
     fequencey { Faker::Subscription.payment_term }
-    customer { nil }
-    tea { nil }
   end
 end
