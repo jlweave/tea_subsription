7e6de88d192403d0c743ede2b0a4fc3e53350eca
Lacey Weaver <jlweave89@gmail.com>
2023-04-08 14:22:38 -0600

    Add tests for api requests basic

--- spec/requests/api/v1/customer_request_spec.rb
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -7,5 +7,26 @@ describe "Customer API" do
     get '/api/v1/customers'
 
     expect(response).to be_successful
+
+    customers = JSON.parse(response.body, symbolize_names: true)
+
+    expect(customers.count).to eq(3)
+
+    customers.each do |customer|
+      expect(customer).to have_key(:id)
+      expect(customer[:id]).to be_an(Integer)
+
+      expect(customer).to have_key(:first_name)
+      expect(customer[:first_name]).to be_a(String)
+
+      expect(customer).to have_key(:last_name)
+      expect(customer[:last_name]).to be_a(String)
+
+      expect(customer).to have_key(:email)
+      expect(customer[:email]).to be_a(String)
+
+      expect(customer).to have_key(:address)
+      expect(customer[:address]).to be_a(String)
+    end    
   end
 end
