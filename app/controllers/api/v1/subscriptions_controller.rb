69ff062b292ada571bdb81330b6db7c5b97cb8c2
Lacey Weaver <jlweave89@gmail.com>
2023-04-12 08:44:41 -0600

    Add index and create method

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,29 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  def create
+    subscription = Subscription.new(sub_params)
+    subscription.status = 'active'
+    if subscription.save
+      render json: SubscriptionSerializer.new_subscription(subscription), status: 201
+    else
+      render json: {errors: "Subscription was not created"}, status: 404
+    end
+  end
+  # def update
+  #   if Item.update(params[:id], item_params).save&&Item.exists?(params[:id])
+  #     render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  #   else
+  #     render json: {errors: "Item was not created"}, status: 404
+  #   end
+  # end
   def index
-     render json: Subscription.all
+    customer = Customer.find(params[:customer_id])
+    subs = customer.subscriptions
+    render json: SubscriptionSerializer.all_subscriptions(subs)
+  end
+
+  private
+
+  def sub_params
+    params.permit(:title, :price, :frequency, :status, :customer_id, :tea_id)
   end
 end
